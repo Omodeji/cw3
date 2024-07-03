c6760e443b32261e791ca2a4cdfb2ffa155bf2aa
Amodu Omobolaji <160015832+Omodeji@users.noreply.github.com>
2024-07-03 23:20:28 +0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,63 @@
+// /* eslint-disable no-console */
+ 
+// import { register } from 'service-worker.js'
+ 
+// if (process.env.NODE_ENV === 'production') {
+//   register(`${process.env.BASE_URL}service-worker.js`, {
+//     ready () {
+//       console.log(
+//         'App is being served from cache by a service worker.\n' +
+//         'For more details, visit https://goo.gl/AFskqB'
+//       )
+//     },
+//     registered () {
+//       console.log('Service worker has been registered.')
+//     },
+//     cached () {
+//       console.log('Content has been cached for offline use.')
+//     },
+//     updatefound () {
+//       console.log('New content is downloading.')
+//     },
+//     updated () {
+//       console.log('New content is available; please refresh.')
+//     },
+//     offline () {
+//       console.log('No internet connection found. App is running in offline mode.')
+//     },
+//     error (error) {
+//       console.error('Error during service worker registration:', error)
+//     }
+//   })
+// }
+
+var cacheName = 'petstore-v1';
+var cacheFiles = [
+    'index.html',
+    'webstore.webmanifest',
+];
+
+self.addEventListener('install', (e) => {
+    console.log('service-worker')
+    e.waitUntil(
+        caches.open(cacheName).then((cache) =>{
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles)
+        })
+    );
+});
+
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(r) {
+            // Download the file if it is not in the cache
+            return r || fetch(e.request).then(function(response) {
+                // Add the new file to cache
+                return caches.open(cacheName).then(function(cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
